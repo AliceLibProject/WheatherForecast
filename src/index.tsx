95e2238c2b7881f5b2e1fa36397db02beb50a151
Pavel <pav@mail.com>
2026-04-15 14:47:29 +0300

    Fix component

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,8 @@
-import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 
-import Weather from './components/Weather';
 
 import './main.scss';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Weather />);
+root.render(<App />);
